f738213734983d3dae68354e1627b9307e3913d2
Rodny Estrada <rodnyecode@gmail.com>
2025-02-11 23:44:11 -0500

    feat: update main function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include "Logic/data.h"
+#include "Interface/menu.h"
+#include "Logic/logic.h"
 
 int main() {
-  printf("QT Empty yet!");
+  InterYearTeam teams[5];
+  char sports[][SPORT_S] = {
+    "fútbol",
+    "baloncesto",
+    "voleibol",
+    "béisbol",
+    "dominó",
+    "ajedrez"
+  };
+  
+  clearTeams(teams);
+  displayAppInfo();
+  displayMainMenu(teams);
   return 0;
 }
